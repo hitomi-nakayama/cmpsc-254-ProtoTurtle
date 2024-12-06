eda7e511f3d17f1ee9f491710b3d06b4bbbbb969
Hitomi Nakayama <hitominakayama1.618@gmail.com>
2024-12-06 13:29:11 -0800

    added transition states

--- pyrtl-accumulator/accumulator/states.py
+++ pyrtl-accumulator/accumulator/states.py
@@ -5,3 +5,36 @@ class OneHotEnum(Enum):
     @staticmethod
     def _generate_next_value_(name, start, count, last_values):
         return 2 ** count
+
+
+class SessionStates(OneHotEnum):
+    LOOP_CHOICE = auto()
+    DATATYPE_CHOICE = auto()
+    ADDEND_U8 = auto()
+    ADDEND_I16 = auto()
+    RESULT = auto()
+
+
+class SessionTransitions(OneHotEnum):
+    LOOP_YES = auto()
+    U8 = auto()
+    U8_TO_LOOP = auto()
+    I16 = auto()
+    I16_TO_LOOP = auto()
+    LOOP_NO = auto()
+    END = auto()
+
+
+def make_session_transition_table():
+    global SESSION_TRANSITION_TABLE
+
+    S = SessionStates
+    T = SessionTransitions
+    return [
+        (),
+        (),
+        (),
+        (),
+    ]
+
+SESSION_TRANSITION_TABLE = make_session_transition_table()
