23c296c0f34a13eda0889a9638f81598868a5547
Hitomi Nakayama <hitominakayama1.618@gmail.com>
2024-12-06 20:12:12 -0800

    added accumulator user logic

--- pyrtl-accumulator/accumulator/states.py
+++ pyrtl-accumulator/accumulator/states.py
@@ -2,6 +2,7 @@ from enum import auto, Enum
 from dataclasses import dataclass
 
 import pyrtl
+from pyrtl import conditional_assignment, WireVector
 
 from .fsm import FSM
 
@@ -13,24 +14,30 @@ class OneHotEnum(Enum):
 
 
 class SessionTransitions(Enum):
-    WAIT = 0  # added because original is syncronous (I think??)
-    LOOP_YES = auto()
+    WAIT = 0  # added to make the fsm asynchronous
     U8 = auto()
     U8_TO_LOOP = auto()
     I16 = auto()
     I16_TO_LOOP = auto()
-    LOOP_NO = auto()
+    GET_RESULT = auto()
     END = auto()
     RESET = auto()
 
 
-SESSION_TRANSITIONS_BITWIDTH = 4
+SESSION_TRANSITION_BITWIDTH = 4
+
+class SessionStates(Enum):
+    CHOICE = 0
+    ADDU = auto()
+    ADDI = auto()
+    RESULT = auto()
+
+SESSION_STATE_NAMES = [x.name for x in SessionStates]
 
 
-SESSION_STATES = ['LOOPCHOICE', 'DTYPECHOICE', 'ADDU', 'ADDI',
-                  'RESULT']
+SESSION_STATE_BITWIDTH = 2
 
-
+NEW_STATE_BITWIDTH = 1
 
 def make_fsm(output_bitwidth: int, output_values: dict[str, int]):
     """The highest bit of the output is high when it is a new state.
@@ -46,58 +53,64 @@ def make_fsm(output_bitwidth: int, output_values: dict[str, int]):
 
     T = SessionTransitions
     rules = [
-        f'LOOPCHOICE + {v(T.LOOP_YES)} -> DTYPECHOICE, {new_val("DTYPECHOICE")}',
-        f'LOOPCHOICE + {v(T.LOOP_NO)} -> RESULT, {new_val("RESULT")}',
-        f'DTYPECHOICE + {v(T.U8)} -> ADDU, {new_val("ADDU")}',
-        f'DTYPECHOICE + {v(T.I16)} -> ADDI, {new_val("ADDI")}',
-        f'ADDU + {v(T.U8_TO_LOOP)} -> LOOPCHOICE, {new_val("LOOPCHOICE")}',
-        f'ADDI + {v(T.I16_TO_LOOP)} -> LOOPCHOICE, {new_val("LOOPCHOICE")}',
-        f'RESULT + {v(T.END)} -> LOOPCHOICE, {new_val("LOOPCHOICE")}',
+        # the states don't have underscores or numbers due to a limitation of
+        # fsm.py that we don't have time to fix right now.
+        f'CHOICE + {v(T.U8)} -> ADDU, {new_val("ADDU")}',
+        f'CHOICE + {v(T.I16)} -> ADDI, {new_val("ADDI")}',
+        f'CHOICE + {v(T.GET_RESULT)} -> RESULT, {new_val("RESULT")}',
+        f'ADDU + {v(T.U8_TO_LOOP)} -> CHOICE, {new_val("CHOICE")}',
+        f'ADDI + {v(T.I16_TO_LOOP)} -> CHOICE, {new_val("CHOICE")}',
+        f'RESULT + {v(T.END)} -> CHOICE, {new_val("CHOICE")}',
 
         # We add the wait transitions because we want our circuit design to be
         # async.
-        f'LOOPCHOICE + {v(T.WAIT)} -> LOOPCHOICE, {old_val("LOOPCHOICE")}',
-        f'DTYPECHOICE + {v(T.WAIT)} -> DTYPECHOICE, {old_val("DTYPECHOICE")}',
+        f'CHOICE + {v(T.WAIT)} -> CHOICE, {old_val("CHOICE")}',
         f'ADDU + {v(T.WAIT)} -> ADDU, {old_val("ADDU")}',
         f'ADDI + {v(T.WAIT)} -> ADDI, {old_val("ADDI")}',
-        f'RESULT + {v(T.WAIT)} -> LOOPCHOICE, {old_val("RESULT")}',
+        f'RESULT + {v(T.WAIT)} -> RESULT, {old_val("RESULT")}',
 
         # this is a hack so that we get the "new state" bit upon resetting.
-        f'all + {v(T.RESET)} -> LOOPCHOICE, {new_val("LOOPCHOICE")}'
+        f'all + {v(T.RESET)} -> CHOICE, {new_val("CHOICE")}'
     ]
 
-    return FSM(states=SESSION_STATES,
-               input_bitwidth=SESSION_TRANSITIONS_BITWIDTH,
+    return FSM(states=SESSION_STATE_NAMES,
+               input_bitwidth=SESSION_TRANSITION_BITWIDTH,
                output_bitwidth=output_bitwidth + 1, rulesList=rules)
 
 
 def main():
-    input_wire = pyrtl.Input(bitwidth=SESSION_TRANSITIONS_BITWIDTH, name="input")
-    state = pyrtl.Output(bitwidth=3, name="currentState")
-    output = pyrtl.Output(bitwidth=4, name="output")
+    input_wire = pyrtl.Input(bitwidth=SESSION_TRANSITION_BITWIDTH, name="input")
+    state = pyrtl.Output(bitwidth=SESSION_STATE_BITWIDTH, name="currentState")
+    new_state = pyrtl.Output(bitwidth=1, name="newState")
+
+    fsm_output_bitwidth = 3
+    output = pyrtl.Output(bitwidth=fsm_output_bitwidth, name="output")
+
     output_values = {
-            'LOOPCHOICE': 1,
-            'DTYPECHOICE': 2,
+            'CHOICE': 1,
             'ADDU': 3,
             'ADDI': 4,
             'RESULT': 5
     }
+
     session_fsm = make_fsm(output_bitwidth=3, output_values=output_values)
     session_fsm <<= input_wire
 
-    output <<= session_fsm()[0]
+    output <<= session_fsm()[0][0:-1]
+    new_state <<= session_fsm()[0][-1]  # the new_state flag is the msb
+
     state <<= session_fsm()[1]
 
     sim_trace = pyrtl.SimulationTrace()
     sim = pyrtl.Simulation(tracer=sim_trace)
     T = SessionTransitions
 
-    input_inputs = [T.RESET, T.WAIT, T.WAIT, T.LOOP_YES, T.U8, T.WAIT, T.U8_TO_LOOP, T.LOOP_NO, T.END, T.WAIT, T.WAIT]
+    input_inputs = [T.RESET, T.WAIT, T.WAIT, T.U8, T.WAIT, T.U8_TO_LOOP, T.GET_RESULT, T.WAIT, T.END, T.WAIT, T.WAIT]
     sim_inputs = {
         'input' : [x.value for x in input_inputs]
     }
     sim.step_multiple(sim_inputs)
-    sim_trace.render_trace(trace_list=['input', 'currentState', 'output'])
+    sim_trace.render_trace(trace_list=['input', 'currentState', 'output', 'newState'])
 
 
 if __name__ == '__main__':
---
+++ pyrtl-accumulator/accumulator/user_logic.py
@@ -0,0 +1,88 @@
+"""This is the logic that the user would normally implement themselves"""
+
+from pyrtl import conditional_assignment, Input, Output, Register, WireVector
+
+from .states import NEW_STATE_BITWIDTH, SessionStates, SessionTransitions, SESSION_STATE_BITWIDTH
+
+
+ACCUMULATOR_REG_BITWIDTH = 16
+ACCUMULATOR_IN_DATA_BITWIDTH = 16
+ACCUMULATOR_OUT_DATA_BITWIDTH = 16
+
+CLIENT_IN_DATA_BITWIDTH = ACCUMULATOR_OUT_DATA_BITWIDTH
+CLIENT_OUT_DATA_BITWIDTH = ACCUMULATOR_IN_DATA_BITWIDTH
+
+class AccumulatorLogic:
+    def __init__(self, name=''):
+        self.fsm_state = WireVector(bitwidth=SESSION_STATE_BITWIDTH,
+                                    name=concat_name('accumulator_fsm_state', name))
+        self.new_state = WireVector(bitwidth=NEW_STATE_BITWIDTH,
+                                    name=concat_name('accumulator_new_state', name))
+        self.send_data = WireVector(bitwidth=ACCUMULATOR_OUT_DATA_BITWIDTH,
+                                    name=concat_name('accumulator_send_data', name))
+        self.recv_data = WireVector(bitwidth=ACCUMULATOR_IN_DATA_BITWIDTH,
+                                    name=concat_name('accumulator_recv_data', name))
+        self.accumulator = Register(ACCUMULATOR_REG_BITWIDTH,
+                                    name=concat_name('accumulator_reg', name),
+                                    reset_value=0)
+
+        # on session
+        with conditional_assignment:
+            with self.fsm_state == SessionStates.CHOICE.value:
+                pass
+            with self.fsm_state == SessionStates.ADDU.value:
+                self.accumulator.next |= self.accumulator + self.recv_data[:8]
+            with self.fsm_state == SessionStates.ADDI.value:
+                self.accumulator.next |= self.accumulator + self.recv_data
+            with self.fsm_state == SessionStates.RESULT.value:
+                self.send_data |= self.accumulator
+                self.accumulator.next |= 0
+
+
+
+def concat_name(base_name: str, suffix: str | None = None):
+    if suffix:
+        return base_name + '_' + suffix
+    return base_name
+
+
+def main():
+    from pyrtl import Simulation
+
+
+    acc_logic = AccumulatorLogic()
+
+    fsm_state = Input(bitwidth=len(acc_logic.fsm_state), name='fsm_state')
+    acc_logic.fsm_state <<= fsm_state
+
+    new_state = Input(bitwidth=len(acc_logic.new_state), name='new_state')
+    acc_logic.new_state <<= new_state
+
+    recv_data = Input(bitwidth=len(acc_logic.recv_data), name='recv_data')
+    acc_logic.recv_data <<= recv_data
+
+    send_data = Output(bitwidth=len(acc_logic.send_data), name='send_data')
+    send_data <<= acc_logic.send_data
+
+    sim = Simulation()
+
+    def v(x):
+        return x.value
+
+    S = SessionStates
+    sim.step_multiple(
+        provided_inputs={
+            'new_state': [1, 1, 1, 1, 1],
+            'fsm_state': [v(S.CHOICE), v(S.ADDU), v(S.ADDI),
+                                      v(S.RESULT), v(S.CHOICE)],
+            'recv_data': [0, 5, -2, 1, 0]
+
+        },
+        expected_outputs={
+            'accumulator_send_data': [0, 0, 0, 3, 0]
+        }
+    )
+
+
+if __name__ == '__main__':
+    main()
